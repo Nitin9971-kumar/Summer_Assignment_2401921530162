fbdf0277606551bf659b556182c2eb24860d9b04
Nitin Kumar <nitinkumarnk892061@gmail.com>
2026-06-12 23:17:36 +0530

    Create groupAnagram.cpp

---
+++ week_2/Day5/groupAnagram.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    unordered_map<string, vector<string>> mp;
+
+    for (string str : strs) {
+        string temp = str;
+        sort(temp.begin(), temp.end());
+
+        mp[temp].push_back(str);
+    }
+
+    vector<vector<string>> ans;
+
+    for (auto it : mp) {
+        ans.push_back(it.second);
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+    vector<vector<string>> result = groupAnagrams(strs);
+
+    for (auto group : result) {
+        cout << "[ ";
+        for (string word : group) {
+            cout << word << " ";
+        }
+        cout << "]" << endl;
+    }
+
+    return 0;
+}
