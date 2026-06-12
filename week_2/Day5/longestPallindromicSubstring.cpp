a18d21a3daadf28889b265ef3fa831fb6ec9fe74
Nitin Kumar <nitinkumarnk892061@gmail.com>
2026-06-12 23:23:03 +0530

    Implement longest palindromic substring function

---
+++ week_2/Day5/longestPallindromicSubstring.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string longestPalindrome(string s) {
+    int n = s.length();
+
+    if (n <= 1)
+        return s;
+
+    int start = 0;
+    int maxLen = 1;
+
+    for (int i = 0; i < n; i++) {
+
+        // Odd length palindrome
+        int left = i;
+        int right = i;
+
+        while (left >= 0 && right < n && s[left] == s[right]) {
+            int len = right - left + 1;
+
+            if (len > maxLen) {
+                maxLen = len;
+                start = left;
+            }
+
+            left--;
+            right++;
+        }
+
+        // Even length palindrome
+        left = i;
+        right = i + 1;
+
+        while (left >= 0 && right < n && s[left] == s[right]) {
+            int len = right - left + 1;
+
+            if (len > maxLen) {
+                maxLen = len;
+                start = left;
+            }
+
+            left--;
+            right++;
+        }
+    }
+
+    return s.substr(start, maxLen);
+}
+
+int main() {
+    string s = "babad";
+
+    cout << "Longest Palindromic Substring: "
+         << longestPalindrome(s);
+
+    return 0;
+}
