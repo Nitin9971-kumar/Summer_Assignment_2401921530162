2fad846c0aa35829a35614bf45928d91896ed1ba
Nitin Kumar <nitinkumarnk892061@gmail.com>
2026-06-15 20:42:46 +0530

    Create middleOfTheLinkedList.cpp

---
+++ week_3/Day1/middleOfTheLinkedList.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode* next;
+
+    ListNode(int data) {
+        val = data;
+        next = nullptr;
+    }
+};
+
+ListNode* middleNode(ListNode* head) {
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
+
+int main() {
+    // 1 -> 2 -> 3 -> 4 -> 5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    ListNode* middle = middleNode(head);
+
+    cout << "Middle Node: " << middle->val << endl;
+
+    return 0;
+}
